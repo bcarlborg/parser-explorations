bac04e6a5a2f783ccea84bdb63e5f59bc730fdc0
bcarlborg <bcarlborg@gmail.com>
2023-04-21 15:50:40 -0700

    create new grammars/ directory

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { BON_GRAMMAR } from './extended-chompsky-normal-form-grammars/example-grammars/beaus-object-notation';
-import { generateRandomStringsForECNFGrammar } from './extended-chompsky-normal-form-grammars/utilities/generate-random-strings-for-ecnf-grammar';
+import { BON_GRAMMAR } from './grammars/extended-chompsky-normal-form-grammars/example-grammars/beaus-object-notation';
+import { generateRandomStringsForECNFGrammar } from './grammars/extended-chompsky-normal-form-grammars/utilities/generate-random-strings-for-ecnf-grammar';
 
 generateRandomStringsForECNFGrammar(BON_GRAMMAR, 100);
